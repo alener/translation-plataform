b7f7d3fe65997a795b8cb6883a553f73ef7394d4
Alejandro Nereo Carballo <alener014@gmail.com>
2017-02-21 18:17:48 -0300

    adjusting model in tradu

--- src/translationplatform/translation/models.py
+++ src/translationplatform/translation/models.py
@@ -1,15 +1,23 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class tradu(models.Model):
-    origintitle = models.CharField(max_length=40)
-    origintext = models.TextField()
-    publicdate = models.DateField(default=True)
-    transtitle = models.CharField(max_length=40)
-    transtext = models.TextField()
-    transdate = models.DateTimeField(auto_now=True,)
+    origintitle = models.CharField("título original",max_length=40)
+    origintext = models.TextField("texto original")
+    publicdate = models.DateField("fecha de la publicación",default=timezone.now)
+    publilink = models.URLField("link a la publicación",null= True, blank=True)
+    transtitle = models.CharField("título traducido",max_length=40)
+    transtext = models.TextField("texto traducido")
+    transdate = models.DateTimeField("comienzo de la traducción",auto_now_add=True, )
+    transupdate = models.DateTimeField("última actualización de la traducción",default=timezone.now)
+    translink = models.URLField("link a la traducción",null=True, blank=True)
 
 
     def __str__(self):
         return self.transtitle
+
+    class META:
+        verbose_name='traducción'
+        verbose_name_plural="traducciones"
+
