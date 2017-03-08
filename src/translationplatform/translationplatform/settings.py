a8889fa22729160bbab219f25876e8f051ede184
Alejandro Nereo Carballo <alener014@gmail.com>
2017-03-08 18:28:01 -0300

    added some objects tradus

--- src/translationplatform/translationplatform/settings.py
+++ src/translationplatform/translationplatform/settings.py
@@ -121,4 +121,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    #'/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn" )
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn" )
